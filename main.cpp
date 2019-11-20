3f6370e2df54d3ee93b4dbdff5737ce92e119583
winkhanh <giakhanhnk2000@gmail.com>
2019-11-20 15:43:36 -0500

    add files

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using std::cout;
+int main(int argc, char* argv[]){    
+    std::string territoryIFile=argv[1];
+    std::string salerepIOFile=argv[2];
+    std::string transactionIFile=argv[3];
+    std::string territoryOFile=argv[4];
+}
---
+++ territory.h
@@ -0,0 +1,10 @@
+#include <string>
+class Territory{
+    private:
+        std::string id;
+        bool ifPremium;
+        long long amount;
+    public:
+        Territory(std::string id,std::string typeOfTerritory);
+        
+}
